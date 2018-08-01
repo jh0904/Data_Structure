b41a21aa8f02dfd57419b948e9a37d42d075a0b6
jh0904 <464062919@qq.com>
2018-08-01 08:58:00 +0800

    第一次上传，创建Git

---
+++ src/Day01_Array/Demo1.java
@@ -0,0 +1,15 @@
+package Day01_Array;
+
+/**
+ * Day01_Array
+ *
+ * @author jh
+ * @date 2018/8/1 8:53
+ * description:数组的简单学习
+ * 把数据码成一排进行存放。单个数据类型，索引
+ */
+public class Demo1 {
+	public static void main(String[] args) {
+
+	}
+}
