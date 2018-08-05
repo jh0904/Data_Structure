d55eb0e623c9fd93baed8c4579d556d479d57661
jh0904 <464062919@qq.com>
2018-08-05 12:59:54 +0800

    添加了二叉搜索树删除指定的元素e。

--- src/day05_BinarySearchTree/BST.java
+++ src/day05_BinarySearchTree/BST.java
@@ -119,14 +119,15 @@ public class BST<E extends Comparable<E>> {
 			Node cur = stack.pop ();
 			System.out.println (cur.e);
 
-			if(cur.right!=null) {
+			if (cur.right != null) {
 				stack.push (cur.right);
 			}
-			if(cur.left!=null){
+			if (cur.left != null) {
 				stack.push (cur.left);
 			}
 		}
 	}
+
 	//中序遍历 遍历出的结果就是排序后的结果。
 	public void inOrder() {
 		inOrder (root);
@@ -159,97 +160,146 @@ public class BST<E extends Comparable<E>> {
 	//层序遍历  广度优先遍历
 	//广度优先遍历意义:更快的找到问题的解,常用于算法设计中-  无权图 最短路径.
 	//图中也具有深度优先遍历和广度优先遍历
-	public void levelOrder(){
+	public void levelOrder() {
 		Queue<Node> q = new LinkedList<> ();
 		q.add (root);
-		while (!q.isEmpty ()){
+		while (!q.isEmpty ()) {
 			Node cur = q.remove ();
 			System.out.println (cur.e);
 
-			if(cur.left!=null){
+			if (cur.left != null) {
 				q.add (cur.left);
 			}
-			if(cur.right!=null){
-				q.add(cur.right);
+			if (cur.right != null) {
+				q.add (cur.right);
 			}
 		}
 
 	}
-	// 寻找二分搜索树的最小元素
-	public E minimum(){
-		if(size == 0)
-			throw new IllegalArgumentException("BST is empty");
 
-		Node minNode = minimum(root);
+	// 寻找二分搜索树的最小元素
+	public E minimum() {
+		if (size == 0)
+			throw new IllegalArgumentException ("BST is empty");
+
+		Node minNode = minimum (root);
 		return minNode.e;
 	}
 
 	// 返回以node为根的二分搜索树的最小值所在的节点  一直去查找左节点,就是最小的值.
-	private Node minimum(Node node){
-		if( node.left == null )
+	private Node minimum(Node node) {
+		if (node.left == null)
 			return node;
 
-		return minimum(node.left);
+		return minimum (node.left);
 	}
 
 	// 寻找二分搜索树的最大元素
-	public E maximum(){
-		if(size == 0)
-			throw new IllegalArgumentException("BST is empty");
+	public E maximum() {
+		if (size == 0)
+			throw new IllegalArgumentException ("BST is empty");
 
-		return maximum(root).e;
+		return maximum (root).e;
 	}
 
 	// 返回以node为根的二分搜索树的最大值所在的节点
-	private Node maximum(Node node){
-		if( node.right == null )
+	private Node maximum(Node node) {
+		if (node.right == null)
 			return node;
 
-		return maximum(node.right);
+		return maximum (node.right);
 	}
 
 	// 从二分搜索树中删除最小值所在节点, 返回最小值
-	public E removeMin(){
-		E ret = minimum();
-		root = removeMin(root);
+	public E removeMin() {
+		E ret = minimum ();
+		root = removeMin (root);
 		return ret;
 	}
 
 	// 删除掉以node为根的二分搜索树中的最小节点
 	// 返回删除节点后新的二分搜索树的根
-	private Node removeMin(Node node){
+	private Node removeMin(Node node) {
 
-		if(node.left == null){
+		if (node.left == null) {
 			Node rightNode = node.right;
 			node.right = null;
-			size --;
+			size--;
 			return rightNode;
 		}
 
-		node.left = removeMin(node.left);
+		node.left = removeMin (node.left);
 		return node;
 	}
 
 	// 从二分搜索树中删除最大值所在节点
-	public E removeMax(){
-		E ret = maximum();
-		root = removeMax(root);
+	public E removeMax() {
+		E ret = maximum ();
+		root = removeMax (root);
 		return ret;
 	}
 
 	// 删除掉以node为根的二分搜索树中的最大节点
 	// 返回删除节点后新的二分搜索树的根
-	private Node removeMax(Node node){
+	private Node removeMax(Node node) {
 
-		if(node.right == null){
+		if (node.right == null) {
 			Node leftNode = node.left;
 			node.left = null;
-			size --;
+			size--;
 			return leftNode;
 		}
 
-		node.right = removeMax(node.right);
+		node.right = removeMax (node.right);
 		return node;
+	}
+
+	//删除指定元素e的节点
+	public void remove(E e) {
+		root = remove (root,e);
+
+	}
+	//递归算法,删除以root为根的e元素节点.
+	private Node remove(Node node, E e) {
+		if(node==null){
+			return null;
+		}
+		if(e.compareTo (node.e)<0){
+			remove (node.left,e);
+			return node;
+		}else if(e.compareTo (node.e)>0){
+			remove (node.right,e);
+			return node;
+		}else{
+
+			//e.compareTo(node.e)==0
+
+			//待删除节点左子树为空
+			if(node.left==null){
+				Node right = node.right;
+				node.right=null;
+				size--;
+				return right;
+			}
+			//待删除节点右子树为空
+			if(node.right==null){
+				Node leftNode = node.left;
+				node.left=null;
+				size--;
+				return leftNode;
+			}
+
+			//待删除节点左右都不为空的情况
+			//找到比待删除节点大的最小节点,即待删除节点右子树的最小节点
+			//用这个节点顶替待删除节点的位置
+			Node successor=minimum (node.right);
+			successor.right=removeMin (node.right);
+			//size++;
+			successor.left=node.left;
+			node.left=node.right=null;
+			//size--;
+			return successor;
+		}
 	}
 
 	@Override
--- src/day05_BinarySearchTree/Main.java
+++ src/day05_BinarySearchTree/Main.java
@@ -25,6 +25,11 @@ public class Main {
 		//bst.preOrderNR ();
 		//System.out.println(bst);
 		//bst.levelOrder ();
+		/*
+		* 查找最大的元素就是一直去寻找他的右节点,知道最后一个右节点的右子树为null时,说明找到最大值,然后返回当前节点.
+		* 删除就是在找到节点为null1的时候,执行删除操作,然后在返回这个元素.
+		*
+		* */
 		bst.minimum ();
 		bst.maximum ();
 	}
