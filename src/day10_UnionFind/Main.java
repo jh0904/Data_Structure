a2e2faaa5c32cc99236569844e6fbada4da5d94d
jh0904 <464062919@qq.com>
2018-08-13 21:40:34 +0800

    第四版的并查集，增加了对rank的优化

--- src/day10_UnionFind/Main.java
+++ src/day10_UnionFind/Main.java
@@ -42,16 +42,19 @@ public class Main {
 //        int m = 10000;
 
 		// UnionFind2 慢于 UnionFind1, 但UnionFind3最快
-		int size = 10000;
-		int m = 10000;
+		int size = 10000000;
+		int m = 10000000;
 
-		UnionFind1 uf1 = new UnionFind1(size);
+		/*UnionFind1 uf1 = new UnionFind1(size);
 		System.out.println("UnionFind1 : " + testUF(uf1, m) + " s");
 
 		UnionFind2 uf2 = new UnionFind2(size);
-		System.out.println("UnionFind2 : " + testUF(uf2, m) + " s");
+		System.out.println("UnionFind2 : " + testUF(uf2, m) + " s");*/
 
 		UnionFind3 uf3 = new UnionFind3(size);
 		System.out.println("UnionFind3 : " + testUF(uf3, m) + " s");
+
+		UnionFind4 uf4 = new UnionFind4(size);
+		System.out.println("UnionFind4 : " + testUF(uf4, m) + " s");
 	}
 }
