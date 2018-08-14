554155ba4415454067b5d98e156f1dc85bbc6ce2
jh0904 <464062919@qq.com>
2018-08-14 22:46:14 +0800

    增加平衡控制的方法，增加了右旋和左旋方法维持平衡二叉树。

--- src/day11_AVL/AVLTree.java
+++ src/day11_AVL/AVLTree.java
@@ -55,16 +55,16 @@ public class AVLTree<K extends Comparable<K>, V> {
 		inOrder (root.right, keys);
 	}
 
-	public boolean isBalanced(){
+	public boolean isBalanced() {
 		return isBalanced (root);
 	}
 
 	private boolean isBalanced(Node node) {
-		if(node==null){
+		if (node == null) {
 			return true;
 		}
-		int balanceFactor=getBalanceFactor (node);
-		if(Math.abs (balanceFactor)>1){
+		int balanceFactor = getBalanceFactor (node);
+		if (Math.abs (balanceFactor) > 1) {
 			return false;
 		}
 		return isBalanced (node.left) && isBalanced (node.right);
@@ -94,6 +94,52 @@ public class AVLTree<K extends Comparable<K>, V> {
 		return size == 0;
 	}
 
+	// 对节点y进行向右旋转操作，返回旋转后新的根节点x
+	//        y                              x
+	//       / \                           /   \
+	//      x   T4     向右旋转 (y)        z     y
+	//     / \       - - - - - - - ->    / \   / \
+	//    z   T3                       T1  T2 T3 T4
+	//   / \
+	// T1   T2
+	private Node rightRotate(Node y) {
+		Node x = y.left;
+		Node T3 = x.right;
+
+		//向右旋转
+		x.right = y;
+		y.left = T3;
+
+		//更新height，先更新y的值
+		y.height = Math.max (getHeight (y.left), getHeight (y.right)) + 1;
+		x.height = Math.max (getHeight (x.left), getHeight (x.right)) + 1;
+
+		return x;
+	}
+
+	// 对节点y进行向左旋转操作，返回旋转后新的根节点x
+	//    y                             x
+	//  /  \                          /   \
+	// T1   x      向左旋转 (y)       y     z
+	//     / \   - - - - - - - ->   / \   / \
+	//   T2  z                     T1 T2 T3 T4
+	//      / \
+	//     T3 T4
+	private Node leftRotate(Node y) {
+		Node x = y.right;
+		Node T2 = x.left;
+
+		//向左旋转
+		x.left = y;
+		y.right = T2;
+
+		//更新height
+		y.height = Math.max (getHeight (y.left), getHeight (y.right)) + 1;
+		x.height = Math.max (getHeight (x.left), getHeight (x.right)) + 1;
+
+		return x;
+	}
+
 	// 向二分搜索树中添加新的元素(key, value)
 	public void add(K key, V value) {
 		root = add (root, key, value);
@@ -120,6 +166,14 @@ public class AVLTree<K extends Comparable<K>, V> {
 		int balanceFactor = getBalanceFactor (node);
 		if (Math.abs (balanceFactor) > 1) {
 			System.out.println ("unbalanced : " + balanceFactor);
+		}
+		//平衡维护
+		if (balanceFactor > 1 && getBalanceFactor (node.left) >= 0) {
+			//右旋转
+			return rightRotate (node);
+		}
+		if (balanceFactor < -1 && getBalanceFactor (node.right) <= 0) {
+			return leftRotate (node);
 		}
 		return node;
 	}
